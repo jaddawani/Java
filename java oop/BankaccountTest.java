2aed6d42d4c609dffc15dea97d8cf9230bbc447f
jad dawani <dawanijad@gmail.com>
2021-06-13 12:57:16 +0300

    oop

---
+++ java oop/BankaccountTest.java
@@ -0,0 +1,20 @@
+public class BankaccountTest{
+
+public static void main(String[] args){
+
+Bankaccount jad = new Bankaccount(1000);
+jad.getbalance();
+jad.setbalance(1000);
+jad.setsavingbalance(5000);
+jad.getsavings();
+
+
+
+
+}
+
+
+
+
+
+}
